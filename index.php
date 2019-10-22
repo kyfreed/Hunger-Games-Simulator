bf99a2c279d3be1831a56c838e38a5908ea67b89
kyfreed <kyle.freed@icloud.com>
2019-10-22 00:00:20 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+        <title>Hunger Games Simulator</title>
+    </head>
+    <body>
+        <h1>Hunger Games Simulator</h1>
+        <h4>Inspired by BrantSteele</h4>
+        <button class="btn btn-primary">Start</button>
+    </body>
+</html>
